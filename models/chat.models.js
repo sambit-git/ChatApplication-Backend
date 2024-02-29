7ef9a7e451ca38e7e785bdfb8a35643987717682
Sambit Kumar <sambit.kumar142@gmail.com>
2024-02-29 18:08:23 +0530

    created remaining models

---
+++ models/chat.models.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const chatSchema = mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    latestMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
+    admin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+const Chat = mongoose.model("Chat", chatSchema);
+
+export default Chat;
